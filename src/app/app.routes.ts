e719e888cd584a372b4c8227a7001b8e6aa93874
Prajakta Hali <prajakta.mahamuni91@gmail.com>
2024-08-07 17:58:39 +0530

    Module Federation microfrontend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'shoppingCartApp',
+    loadChildren: () => {
+      return loadRemoteModule({
+        remoteEntry: "http://localhost:4201/remoteEntry.js",
+        remoteName: 'ShoppingCartApp',
+        exposedModule: './ShoppingCartModule'
+      }).then(m => m.ShoppingCartModule).catch(error =>
+        console.log('Error=>', error));
+    }
+  }
+];
